2ab39b4ca17b078e7afd0830a11feb3f73dd6cde
Andrey7967 <64693383+Andrey7967@users.noreply.github.com>
2025-09-05 01:29:11 +0300

    Update db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,11 +4,12 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 import { Pool } from 'pg';
  const pool = new Pool({
-  user:  process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database:process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_Port),
+  connectionString: process.env.DATABASE_URL,
+ 
+  
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 
